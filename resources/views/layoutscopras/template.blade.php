efa16b7489fe7ee305f351eed6f5beb923abb80e
paksi <paksi.dh@gmail.com>
2024-06-13 09:54:12 +0700

    Edit title

--- resources/views/layoutscopras/template.blade.php
+++ resources/views/layoutscopras/template.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Spk Copras</title>
+    <title>Copras - RTKAS</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{ asset('staradmin/assets/vendors/feather/feather.css') }}">
     <link rel="stylesheet" href="{{ asset('staradmin/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
